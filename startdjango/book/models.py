28a0e6ce009a615677a55dac3bf8934052e246ab
lettuce lu <lettuce.lu@lettucelu.com>
2025-05-25 00:16:45 +0800

    # 創建 user model

--- startdjango/book/models.py
+++ startdjango/book/models.py
@@ -6,3 +6,13 @@ class Book(models.Model):
     author = models.CharField(max_length=20, null=False)
     pub_time = models.DateTimeField(auto_now_add=True)
     price = models.FloatField(default=0)
+
+
+class Author(models.Model):
+    is_active = models.BooleanField()
+    username = models.CharField(max_length=200)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    visit_count = models.IntegerField()
+    profile = models.TextField()
+    website = models.URLField()
