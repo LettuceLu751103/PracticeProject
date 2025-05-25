9401643746c4af360f83a965ba517cd03793be68
lettuce lu <lettuce.lu@lettucelu.com>
2025-05-25 13:43:08 +0800

    # 在 app bookstore 中, 創建 Author, Book Model

--- startdjango/bookstore/models.py
+++ startdjango/bookstore/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    authorname = models.CharField(max_length=200)
+    authorphone = models.CharField(max_length=14)
+    created_time = models.DateTimeField(auto_now=True)
+    
+class Book(models.Model):
+    bookname = models.CharField(max_length=200),
+    original_price = models.IntegerField()
+    discount = models.FloatField()
+    final_price = models.IntegerField()
+    pub_time = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
