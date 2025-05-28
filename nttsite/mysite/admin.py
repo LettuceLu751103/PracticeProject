b37fced48c7f59a6e464a6e2afa180411e663e92
lettuce lu <lettuce.lu@lettucelu.com>
2025-05-28 23:01:12 +0800

    # 新增 Userinfo class, 並且在 admin.py 中註冊此 class

--- nttsite/mysite/admin.py
+++ nttsite/mysite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Userinfo
 
 # Register your models here.
+# ex: admin.site.register(model class)
+admin.site.register(Userinfo)
--- nttsite/mysite/models.py
+++ nttsite/mysite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Userinfo(models.Model):
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nickname} ({self.email})"
