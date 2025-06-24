2fca8759ac1a8510cbbe35833fe8ad1a4bce7c24
lettuce lu <lettuce.lu@lettucelu.com>
2025-06-24 23:52:18 +0800

    # VUE Day01

---
+++ front-end/vue-base/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>{{ msg }}</p>
+  <p>Author 名字 : {{ names }}</p>
+  <div>
+    <p v-for="member in members">Team Member: {{ member }}</p>
+  </div>
+  <div>
+    <p>數字相加 {{ number + 10}}</p>
+  </div>
+  <div>
+    <p>狀態變化 {{flag ? '正確' : '錯誤'}}</p>
+  </div>
+  <div>
+    <p>Author 名字反轉 : {{ names.split('').reverse().join('') }}</p>
+  </div>
+  <p>{{ rawHTML }}</p>
+  <p v-html="rawHTML"></p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '神奇的VUE框架',
+      names: 'Lettuce Lu',
+      members: ['Helen', 'Ruby', 'Brian', 'Horace'],
+      number: 5,
+      flag: false,
+      rawHTML: '<a href="https://www.google.com">百戰生菜</a>'
+    }
+  }
+}
+</script>
---
+++ front-end/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-bind:id="dynamicID" v-bind:name="dynamicNAME" v-bind:class="msg">測試</div>
+  <button v-bind:disabled="flag">登入按鈕 可用狀態</button>
+  <div>
+    <button v-bind:disabled="state">登入按鈕 不可用狀態</button>
+  </div>
+  <div v-bind="attr">
+    使用對象的文字, 具有 id, name
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'appClass',
+      dynamicID: 'appID',
+      dynamicNAME: 'appNAME',
+      flag: false,
+      state: true,
+      attr:{
+        class: 'redClass',
+        id: 'redId'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.appClass {
+  color: blue;
+  font-size: 26px;
+}
+.redClass {
+  color: red;
+  font-size: 30px;
+}
+</style>
