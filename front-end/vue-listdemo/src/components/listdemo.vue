0cd467b5e816572edb2e980b6a332fe7939eb267
lettuce lu <lettuce.lu@lettucelu.com>
2025-06-25 22:23:10 +0800

    # key 管理

--- front-end/vue-listdemo/src/components/listdemo.vue
+++ front-end/vue-listdemo/src/components/listdemo.vue
@@ -10,7 +10,8 @@
         </ul>
     </div>
     <div>
-        <p v-for="(member, index) in members" key="index">當前索引 {{ key }} - {{ member }}</p>
+        <h4>Key 屬性添加到 v-for 當中</h4>
+        <p v-for="(number, index) in numbers" :key='index'>當前索引 {{ index }} - {{ number }}</p>
     </div>
     
 </template>
@@ -25,7 +26,8 @@
                     { ip:'192.168.10.15', brand:'Cisco', model:'ASR1002-HX'},
                     { ip:'10.255.230.77', brand:'Juniper', model:'SRX-345'},
                     { ip:'172.16.55.30', brand:'Cisco', model:'C93180-EX'}
-                ]
+                ],
+                numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             }
         }
     }
