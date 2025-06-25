8f037eadc4e783dbda01b8500e850dce00ea1030
lettuce lu <lettuce.lu@lettucelu.com>
2025-06-25 21:42:14 +0800

    # vue 條件渲染

---
+++ front-end/vue-condition/src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <h3>{{ title }}</h3>
+  <p v-if="state">你看不看的到我</p>
+  <p v-else>前一句看不到, 所以這句看到了</p>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title:'條件渲染',
+      state: true
+    }
+  }
+}
+</script>
