aee341769060b1348141811f25e5d524431da74b
lettuce lu <lettuce.lu@lettucelu.com>
2025-07-09 22:54:02 +0800

    # VUE slot

---
+++ front-end/vue-component-slot/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <SlotBase>
+    <div>
+      <h3>插槽標題</h3>
+      <p>插槽內容</p>
+    </div>
+  </SlotBase>
+</template>
+
+<script>
+import SlotBase from './components/SlotBase.vue'
+export default {
+  data(){
+
+  },
+  components:{
+    SlotBase
+  }
+}
+</script>
